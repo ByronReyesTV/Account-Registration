1be00e05650ff14daccc10edc0967bcf6674db0c
reyes221093 <102719907+reyes221093@users.noreply.github.com>
2022-09-09 14:08:32 +0800

    Add project files.

---
+++ Account Registration/form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Account_Registration
+{
+    public partial class form1 : Form
+    {
+        public static form1 instance;
+        public TextBox tb1;
+        public form1()
+        {
+            InitializeComponent();
+            instance = this;
+            tb1 = textBox1;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            form2 form = new form2();
+            form.Show();
+
+            /* input */
+            form2.instance.lab1.Text = textBox1.Text;
+            form2.instance.lab2.Text = comboBox1.Text;
+            form2.instance.lab3.Text = textBox2.Text;
+            form2.instance.lab4.Text = textBox3.Text;
+            form2.instance.lab5.Text = textBox4.Text;
+            form2.instance.lab6.Text = textBox5.Text;
+            form2.instance.lab7.Text = textBox6.Text;
+            form2.instance.lab8.Text = richTextBox1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            /* Clear */
+            textBox1.Clear();
+            comboBox1.ResetText();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            richTextBox1.Clear();
+        }
+    }
+}
---
+++ Account Registration/form2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Account_Registration
+{
+    public partial class form2 : Form
+    {
+        public static form2 instance;
+
+        /* form 1 and form 2 connectiom */
+        public Label lab1;
+        public Label lab2;
+        public Label lab3;
+        public Label lab4;
+        public Label lab5;
+        public Label lab6;
+        public Label lab7;
+        public Label lab8;
+        public form2()
+        {
+            InitializeComponent();
+            instance = this;
+
+            /* Output */
+            lab1 = label9;
+            lab2 = label10;
+            lab3 = label11;
+            lab4 = label12;
+            lab5 = label13;
+            lab6 = label14;
+            lab7 = label15;
+            lab8 = label16;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
